da0b0ccb855df0886b6dd8a59d390afa15ee23fd
abarraghi <abarraghi@gmail.com>
2024-03-13 13:46:10 -0400

    Implemented sorting by price

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
       <input type="text" placeholder="Search for an Antique" id="search-input">
       <button id="search-btn" @click="searchName()">Search!</button>
       <button id="clear-btn" @click="clearFilters()">Clear!</button>
+
+    <div v-for="(val, key) in sorts">
+      <input type="radio" name="radio" :value="val" v-model="selected" :id="key" @click="orderPrice(key)">
+      <label :for="val">{{ val }}</label>
+    </div>
     </div>
   </section>
   <section>
@@ -41,12 +46,37 @@
 
 import axios from 'axios';
 
+function compare(antique1,antique2){
+  if(antique1.price > antique2.price){
+    return 1;
+  }
+  if(antique1.price < antique2.price){
+    return -1;
+  }
+  return 0;
+}
+
   export default { 
     name: "antique",
     data() {
           return{
               antiques: {},
+              sorts: {
+                "1": "Low to High", 
+                "-1": "High to Low"
+              },
+              sortSelected: null
           }
+    },
+    computed: {
+      selected: {
+        get() {
+          return this.sortSelected;
+        },
+        set(v){
+          this.sortSelected = v;
+        }
+      }
     },
     mounted() {
       this.getAntiques();
@@ -69,7 +99,16 @@ import axios from 'axios';
       },
       clearFilters(){
         let input = document.getElementById("search-input").value = "";
+        this.sortSelected = null;
         this.getAntiques();
+      }, 
+      orderPrice(sortType){
+        if(sortType == 1){
+          this.antiques.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+        }
+        else if(sortType == -1){
+          this.antiques.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+        }
       }
     }
 }
